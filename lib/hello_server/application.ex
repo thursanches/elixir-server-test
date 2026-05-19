daad006155d75d62d28f97a5fa22de7500e97edc
Arthur <arthursks7@gmail.com>
2026-05-18 23:29:17 -0300

    elixir server on

---
+++ lib/hello_server/application.ex
@@ -0,0 +1,12 @@
+defmodule HelloServer.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {Plug.Cowboy, scheme: :http, plug: HelloServer, options: [port: 4000]}
+    ]
+
+    opts = [strategy: :one_for_one, name: HelloServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
